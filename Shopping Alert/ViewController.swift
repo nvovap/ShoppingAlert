20563687a7d3d1cb9b9e683db07e1420ce3e8e82
Vladimir Nevinniy <nvovap@gmail.com>
2016-02-19 18:11:17 +0200

    first

--- Shopping Alert/ViewController.swift
+++ Shopping Alert/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
+    @IBOutlet weak var txtAddItem: UITextField!
+    
+    @IBOutlet weak var tblShoppingList: UITableView!
+    
+    @IBOutlet weak var btnAction: UIButton!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    var shoppingList: NSMutableArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tblShoppingList.delegate = self
+        self.tblShoppingList.dataSource = self
+        
+        self.txtAddItem.delegate = self
+        
+        datePicker.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,75 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    // MARK: Implement UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if shoppingList == nil {
+            shoppingList = NSMutableArray()
+        }
+        
+        shoppingList.addObject(textField.text!)
+        
+        tblShoppingList.reloadData()
+        
+        txtAddItem.text = ""
+        txtAddItem.resignFirstResponder()
+        
+        return true
+    }
+    
 
+    // MARK: IBAction method implementation
+
+    @IBAction func scheduleReminder(sender: AnyObject) {
+        
+    }
+    
+    
+    // MARK: UITableView method implementation
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rows = 0
+        
+        if let list = shoppingList {
+            rows = list.count
+        }
+        
+        return rows
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        
+        cell.textLabel?.text = shoppingList[indexPath.row] as? String
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50.0
+    }
+    
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            remoteItemAtIndex(indexPath.row)
+        }
+    }
+    
+    func remoteItemAtIndex(index: Int) {
+        shoppingList.removeObjectAtIndex(index)
+        
+        tblShoppingList.reloadData()
+    }
+    
 }
 
