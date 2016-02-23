18c1d0b0ac50092230bc81a14ce11ff2bb5672a1
Vladimir Nevinniy <nvovap@gmail.com>
2016-02-23 18:08:00 +0200

    second modify

--- Shopping Alert/ViewController.swift
+++ Shopping Alert/ViewController.swift
@@ -31,11 +31,111 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.txtAddItem.delegate = self
         
         datePicker.hidden = true
+        
+        
+        loadShoppingList()
     }
+    
+    
+    @IBAction func sheduleReminder(sender: UIButton) {
+        if datePicker.hidden {
+            animateMyViews(tblShoppingList, viewToShow: datePicker)
+        } else {
+            animateMyViews(datePicker, viewToShow: tblShoppingList)
+        }
+        
+        txtAddItem.enabled = !txtAddItem.enabled
+    }
+    
+    
+    // MARK: - Implementation notification 
+    func setupNotificationSetting() {
+        
+        var justInformAction = UIMutableUserNotificationAction()
+        justInformAction.identifier = "justInform"
+        justInformAction.title = "OK, got it"
+        justInformAction.activationMode = UIUserNotificationActivationMode.Background
+        justInformAction.destructive = false
+        justInformAction.authenticationRequired = false
+        
+        
+        var modifyListAction = UIMutableUserNotificationAction()
+        modifyListAction.identifier = "editList"
+        modifyListAction.title = "Edit list"
+        modifyListAction.activationMode = UIUserNotificationActivationMode.Foreground
+        modifyListAction.destructive = false
+        modifyListAction.authenticationRequired = true
+        
+        
+        var trashAction = UIMutableUserNotificationAction()
+        trashAction.identifier = "trashAction"
+        trashAction.title = "Delete list"
+        trashAction.activationMode = UIUserNotificationActivationMode.Foreground
+        trashAction.destructive = false
+        trashAction.authenticationRequired = true
+        
+        let actionsArray = [justInformAction, modifyListAction, trashAction]
+        let actionsArrayMinimal = [trashAction, modifyListAction]
+        
+        
+        var shoppingListReminderCategory = UIMutableUserNotificationCategory()
+        shoppingListReminderCategory.identifier = "shoppingListReminderCategory"
+        shoppingListReminderCategory.setActions(actionsArray, forContext: UIUserNotificationActionContext.Default)
+        shoppingListReminderCategory.setActions(actionsArrayMinimal, forContext: UIUserNotificationActionContext.Minimal)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Animate Transaction
+    
+    func animateMyViews(viewToHide: UIView, viewToShow: UIView) {
+        let animationDuration = 0.35
+        
+        UIView.animateWithDuration(animationDuration, animations: { () -> Void in
+            viewToHide.transform = CGAffineTransformScale(viewToHide.transform, 0.001, 0.001)
+            
+            }) { (completion: Bool) -> Void in
+                
+                viewToHide.hidden = true
+                viewToShow.hidden = false
+                
+                viewToShow.transform = CGAffineTransformScale(viewToShow.transform, 0.001, 0.001)
+                
+                UIView.animateWithDuration(animationDuration, animations: { () -> Void in
+                    viewToShow.transform = CGAffineTransformIdentity
+                })
+        }
+    }
+    
+    
+    // MARK: - Save and load data on disk
+    func loadShoppingList() {
+        let pathsArray = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory = pathsArray[0]
+        
+        print(documentsDirectory)
+        
+        let loadPath = documentsDirectory.stringByAppendingString("/shopping_list")
+        
+        if NSFileManager.defaultManager().fileExistsAtPath(loadPath) {
+            shoppingList = NSMutableArray(contentsOfFile: loadPath)
+            tblShoppingList.reloadData()
+        }
+        
+       
+        
+    }
+    
+    func saveShoppingList() {
+        let pathsArray = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory = pathsArray[0]
+        
+        print(documentsDirectory)
+        
+        let savePath = documentsDirectory.stringByAppendingString("/shopping_list")
+        
+        shoppingList.writeToFile(savePath, atomically: true)
+        
     }
 
     
@@ -52,6 +152,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         txtAddItem.text = ""
         txtAddItem.resignFirstResponder()
+        
+        saveShoppingList()
         
         return true
     }
@@ -104,6 +206,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func remoteItemAtIndex(index: Int) {
         shoppingList.removeObjectAtIndex(index)
+        
+        saveShoppingList()
         
         tblShoppingList.reloadData()
     }
